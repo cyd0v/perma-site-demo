95208e7d915967a25fd99e2b21e61ec273e44165
Jun Wei Aw <junwei021021@gmail.com>
2025-11-23 21:47:54 +0800

    A

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
+  base: '/perma-site/',
   build: {
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, 'index.html'),
-        donate: path.resolve(__dirname, 'donate.html')
-      }
-    }
-  }
+        donate: path.resolve(__dirname, 'donate.html'),
+      },
+    },
+  },
 });
